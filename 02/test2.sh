ddd0edbde4d0cf8f1c7136bd5b41ad322460d192
Ben <bschreiber91@cs.huji.ac.il>
2020-10-20 22:32:13 +0300

    Project 2: Shift Left and Right implementation

--- 02/test2.sh
+++ 02/test2.sh
@@ -2,7 +2,7 @@
 
 EX=2
 TAR_FILE=project$EX.tar
-FILE_LIST='ALU  Add16  FullAdder  HalfAdder  Inc16'
+FILE_LIST='ALU  Add16  FullAdder  HalfAdder  Inc16 ShiftLeft ShiftRight Mul'
 
 mkdir testTar
 cp $TAR_FILE testTar/
@@ -11,22 +11,11 @@ temp=`pwd`
 
 tar -xf $TAR_FILE
 
-find . -iname \*.hdl -exec cp -n {} . \; # find and copy all chips to the current directory
 for CHIP in $FILE_LIST; do
   if [ ! -r $CHIP.hdl ]; then
     echo Chip $CHIP was not found
   fi
 done
-
-if [ ! -r README ]; then
-  README_FILE=`find . -iname 'README*' -print -quit`
-  if [[ -n $README_FILE ]]; then
-    echo Found $README_FILE, renaming it to README.
-    echo I advise you to resubmit with the proper name.
-    cp $README_FILE README
-    chmod a+r README
-  fi
-fi
 
 if [ -r README ]; then
   dos2unix README &> /dev/null
