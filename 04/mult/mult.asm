0266bb0b82e20e1ca504c0560a75391a0f892b27
yitzchaklindenbaum <yitzchak.lindenbaum@mail.huji.ac.il>
2020-10-30 14:48:15 +0200

    Implemented mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,64 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Algorithm:
+
+// if R1 > R0:
+//    switch(R1, R0)
+
+// R2 = 0
+// for i in range(R1):
+//    R2 += R0
+
+// Check R0 >= R1
+@R1
+D=M
+@R0
+D=D-M
+@INIT_LOOP
+D;JLE
+
+// flip(R1, R0)
+@R1
+D=M
+@temp
+M=D // temp = R1
+
+@R0
+D=M
+@R1
+M=D //  R1 = R0
+
+@temp
+D=M
+@R0
+M=D  //  R0 = temp
+
+(INIT_LOOP)
+    @R2
+    M=0
+    @R1
+    D=M
+    @END
+    D;JEQ
+    @counter
+    M=1
+
+(LOOP)
+    @R0
+    D=M
+    @R2
+    M=M+D // R2 += R0
+
+    @counter
+    M=M+1 // counter++
+    D=M
+    @R1
+    D=M-D
+    @LOOP
+    D;JGE // if counter <= R1, iterate again
+
+
+
+
