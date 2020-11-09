61b75698b33b56fb6aa806059eeefa7ec6120231
yitzchaklindenbaum <yitzchak.lindenbaum@mail.huji.ac.il>
2020-11-09 16:12:36 +0200

    Debugged binary

--- 06/Code.py
+++ 06/Code.py
@@ -20,7 +20,7 @@ class Code:
             pass
 
     def binary(self):
-        return str(bin(int(self.parser.symbol())))  # check that there won't
+        return "{0:15b}".format(self.parser.symbol())  # check that there won't
         # be overload with int
 
     def test(self, string, test):
