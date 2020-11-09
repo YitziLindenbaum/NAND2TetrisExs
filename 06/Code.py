4081db18799188781681af4a9a72867930e2e888
yitzchaklindenbaum <yitzchak.lindenbaum@mail.huji.ac.il>
2020-11-09 17:01:14 +0200

    Improved binary

--- 06/Code.py
+++ 06/Code.py
@@ -15,7 +15,8 @@ class Code:
         if self.parser.commandType() == A_COMMAND:
             return '0' + self.binary()
         elif self.parser.commandType() == C_COMMAND:
-            return '1' + self.dest() + self.comp() + self.jump()
+            ret = '1' + self.comp() + self.dest() + self.jump()
+            return ret
         elif self.parser.commandType() == L_COMMAND:
             pass
 
@@ -28,14 +29,15 @@ class Code:
             string += "1"
         else:
             string += "0"
+        return string
 
     def dest(self):
         dest_asm = self.parser.dest()
         dest_bin = ""
 
-        self.test(dest_bin, "A" in dest_asm)
-        self.test(dest_bin, "D" in dest_asm)
-        self.test(dest_bin, "M" in dest_asm)
+        dest_bin = self.test(dest_bin, "A" in dest_asm)
+        dest_bin = self.test(dest_bin, "D" in dest_asm)
+        dest_bin = self.test(dest_bin, "M" in dest_asm)
 
         return dest_bin
 
@@ -57,42 +59,43 @@ class Code:
             comp_bin += "0"
 
         # instruction[11] (ZX or shift_left)
-        self.test(comp_bin, "D" not in comp_asm or "<<" in comp_asm)
+        comp_bin = self.test(comp_bin, "D" not in comp_asm or "<<" in comp_asm)
 
         # instruction[10] (NX or shift D)
         instruction10_yes = {"1", "-1", "A", "!A", "-A", "D+1", "A+1",
                              "A-1", "D-A", "D|A", "D>>", "D<<"}
-        self.test(comp_bin, comp_asm in instruction10_yes)
+        comp_bin = self.test(comp_bin, comp_asm in instruction10_yes)
 
         # append zeroes and stop if shift command
         if "<<" in comp_asm or ">>" in comp_asm:
             return comp_bin + "0000"
 
         # instruction[9] (ZY)
-        self.test(comp_bin, "A" not in comp_asm)
+        comp_bin = self.test(comp_bin, "A" not in comp_asm)
 
         # instruction[8] (NY)
         instruction8_yes = {"1", "D", "!D", "-D", "D+1", "A+1", "D-1",
                             "A-D", "D|A"}
-        self.test(comp_bin, comp_asm in instruction8_yes)
+        comp_bin = self.test(comp_bin, comp_asm in instruction8_yes)
 
         # instruction[7] (F)
         instruction7_any = {"1", "0", "-", "+"}
-        self.test(comp_bin, any(char in comp_asm for char in instruction7_any))
+        comp_bin = self.test(comp_bin, any(char in comp_asm for char in instruction7_any))
 
         # instruction[6] (NO)
         instruction6_yes = {"1", "!D", "!A", "-D", "-A", "D+1", "A+1", "D-A",
                             "A-D", "D|A"}
-        self.test(comp_bin, comp_asm in instruction6_yes)
+        comp_bin = self.test(comp_bin, comp_asm in instruction6_yes)
 
         return comp_bin
 
     def jump(self):
         jump_asm = self.parser.jump()
         jump_bin = ""
-
-        self.test(jump_bin, jump_asm in {"JLT", "JNE", "JLE", "JMP"})
-        self.test(jump_bin, jump_asm in {"JEQ", "JGE", "JLE", "JMP"})
-        self.test(jump_bin, jump_asm in {"JGT", "JGE", "JNE", "JMP"})
+        print("jump_asm: ", jump_asm)
+        jump_bin = self.test(jump_bin, jump_asm in {"JLT", "JNE", "JLE", "JMP"})
+        jump_bin = self.test(jump_bin, jump_asm in {"JEQ", "JGE", "JLE", "JMP"})
+        jump_bin = self.test(jump_bin, jump_asm in {"JGT", "JGE", "JNE", "JMP"})
+        print("jump_bin: ", jump_bin)
 
         return jump_bin
